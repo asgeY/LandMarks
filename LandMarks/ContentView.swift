0042246ab9883ad09fd641044fe399ae109cd90c
Kritbovorn <kritbovorn@Kritbovorns-MacBook-Pro.local>
2019-06-12 14:40:51 +0700

    Create Project

---
+++ LandMarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  LandMarks
+//
+//  Created by Kritbovorn on 12/6/2562 BE.
+//  Copyright © 2562 Kritbovorn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
