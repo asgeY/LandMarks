6c985c6178fd0041cf5f46ea41300377467873e7
Kritbovorn <kritbovorn@Kritbovorns-MacBook-Pro.local>
2019-06-15 00:32:45 +0700

    Drawing Paths and Shapes

---
+++ LandMarks/Badge.swift
@@ -0,0 +1,47 @@
+//
+//  Badge.swift
+//  LandMarks
+//
+//  Created by Kritbovorn on 14/6/2562 BE.
+//  Copyright © 2562 Kritbovorn. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge : View {
+    
+    static let rotationCount = 8
+    
+    var badgeSymbols: some View {
+        
+        ForEach(0..<Badge.rotationCount) { i in
+            RotatedBadgeSymbol(
+                angle: .degrees(Double(i) / Double(Badge.rotationCount)) * 360.0
+            )
+        }
+        .opacity(0.5)
+    }
+    
+    var body: some View {
+        
+        ZStack {
+            
+            BadgeBackground()
+            
+            GeometryReader { geometry in
+                self.badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+            }
+        }
+        .scaledToFit()
+    }
+}
+
+#if DEBUG
+struct Badge_Previews : PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
+#endif
---
+++ LandMarks/RotatedBadgeSymbol.swift
@@ -0,0 +1,29 @@
+//
+//  RotatedBadgeSymbol.swift
+//  LandMarks
+//
+//  Created by Kritbovorn on 15/6/2562 BE.
+//  Copyright © 2562 Kritbovorn. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol : View {
+    
+    let angle: Angle
+    
+    var body: some View {
+        
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#if DEBUG
+struct RotatedBadgeSymbol_Previews : PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
+#endif
