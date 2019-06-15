798a514274a9e2d3bd16f94fe78c05bc7adb39b9
Kritbovorn <kritbovorn@Kritbovorns-MacBook-Pro.local>
2019-06-15 20:51:21 +0700

    Interfacing with UIKit

---
+++ LandMarks/PageView.swift
@@ -0,0 +1,40 @@
+//
+//  PageView.swift
+//  LandMarks
+//
+//  Created by Kritbovorn on 15/6/2562 BE.
+//  Copyright © 2562 Kritbovorn. All rights reserved.
+//
+
+import SwiftUI
+
+struct PageView<Page: View> : View {
+    
+    var viewControllers: [UIHostingController<Page>]
+    
+    @State var currentPage = 0
+    
+    init(_ views: [Page]) {
+        self.viewControllers = views.map { UIHostingController(rootView: $0)}
+    }
+    
+    var body: some View {
+        
+        ZStack {
+            
+            PageViewController(controllers: viewControllers, currentPage: $currentPage)
+            PageControl(numberOfPages: viewControllers.count, currentPage: $currentPage)
+            .padding(.trailing)
+        }
+    }
+}
+
+#if DEBUG
+struct PageView_Previews : PreviewProvider {
+    static var previews: some View {
+        
+        PageView(features.map{ FeatureCard(landmark: $0) })
+            .aspectRatio(3/2, contentMode: .fit)
+    }
+}
+#endif
